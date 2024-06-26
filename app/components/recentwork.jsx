aa5fa0f52aab75e9eec71aacb273a267c5f0ae72
ash2228 <92314878+ash2228@users.noreply.github.com>
2024-06-26 16:46:32 +0530

    smoothed the scrolling transition

--- app/components/recentwork.jsx
+++ app/components/recentwork.jsx
@@ -1,17 +1,17 @@
 "use client"
 import {Roboto} from "next/font/google";
 import Image from "next/image";
-import { useState,useEffect } from "react";
+import { useState,useEffect, useRef } from "react";
 const roboto = Roboto({weight:"400",subsets:["latin"]});
 export default function Recentwork(){
   const [hover,setHover] = useState(false);
   const [HoverOnWork,setWorkHover] = useState(false);
   const [x,setX] = useState(0);
   const [y,setY] = useState(0);
-  const [first,setFirst] = useState(false);
-  const [second,setSecond] = useState(false);
-  const [three,setThree] = useState(false);
-  const [four,setFour] = useState(false);
+  const first = useRef();
+  const second = useRef();
+  const three = useRef();
+  const four = useRef();
   const hoverHandler = ()=>{
     setHover(!hover);
   }
@@ -19,24 +19,6 @@ export default function Recentwork(){
     setX(e.clientX-500);
     setY(e.clientY-200);
     setWorkHover(true);
-    if(document!==undefined){
-        if(first){
-            let a = document.getElementById("first");
-            a.scrollIntoView({behavior:"smooth"})
-        }
-        if(second){
-            let a = document.getElementById("second");
-            a.scrollIntoView({behavior:"smooth"})
-        }
-        if(three){
-            let a = document.getElementById("three");
-            a.scrollIntoView({behavior:"smooth"})
-        }
-        if(four){
-            let a = document.getElementById("four");
-            a.scrollIntoView({behavior:"smooth"})
-        }
-    }
   }
 
     return(<div className="h-[400vh] xl:mt-20 px-8 mt-52">
@@ -70,28 +52,28 @@ export default function Recentwork(){
       </button>
         </div>
         <div className="flex-col content-center justify-between mt-10 hidden xl:flex" onMouseMove={workHover} onMouseLeave={()=>{setWorkHover(false)}}>
-        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{setFirst(true)}} onMouseLeave={()=>{setFirst(false)}}>
+        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{first.current.scrollIntoView({behavior: 'smooth'})}}>
             <hr className="border w-[80%] m-auto" />
             <div className="py-16 px-60">
                 <span className={`font-bold ${roboto.className} text-8xl`}>Ashlee</span>
                 <span className={`${roboto.className} float-right mt-12`}>Grading & Editing</span>
             </div>
             </div>
-        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{setSecond(true)}} onMouseLeave={()=>{setSecond(false)}}>
+        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{second.current.scrollIntoView({behavior: 'smooth'})}}>
             <hr className="border w-[80%] m-auto" />
             <div className="py-16 px-60">
                 <span className={`font-bold ${roboto.className} text-8xl`}>The Case</span>
                 <span className={`${roboto.className} float-right mt-12`}>Transitions & Editing</span>
             </div>
             </div>
-        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{setThree(true)}} onMouseLeave={()=>{setThree(false)}}>
+        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{three.current.scrollIntoView({behavior: 'smooth'})}}>
             <hr className="border w-[80%] m-auto" />
             <div className="py-16 px-60">
                 <span className={`font-bold ${roboto.className} text-8xl`}>Brand™</span>
                 <span className={`${roboto.className} float-right mt-12`}>Transitions & Editing</span>
             </div>
             </div>
-        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{setFour(true)}} onMouseLeave={()=>{setFour(false)}}>
+        <div className="flex-col content-center justify-between hidden xl:flex" onMouseEnter={()=>{four.current.scrollIntoView({behavior: 'smooth'})}}>
             <hr className="border w-[80%] m-auto" />
             <div className="py-16 px-60">
                 <span className={`font-bold ${roboto.className} text-8xl`}>Jennifer</span>
@@ -108,10 +90,10 @@ export default function Recentwork(){
         </div>
         <div style={{ position: 'fixed', left: x, top: y }} className={`hidden border ${HoverOnWork&&"xl:block scaleDiv"} bg-[#EFEFEF] content-center p-10`} id="slider">
             <div className="h-[200px] overflow-scroll flex flex-col gap-40">
-            <Image src="/thumb2.png" alt="" width={400} height={400} id="first" />
-            <Image src="/football.png" alt="" width={400} height={400} id="second"  />
-            <Image src="/thumb3.png" alt="" width={400} height={400} id="three"  />
-            <Image src="/thumb5.png" alt="" width={400} height={400} id="four"  />
+            <Image src="/thumb2.png" alt="" width={400} height={400} ref={first} />
+            <Image src="/football.png" alt="" width={400} height={400} ref={second}  />
+            <Image src="/thumb3.png" alt="" width={400} height={400} ref={three}  />
+            <Image src="/thumb5.png" alt="" width={400} height={400} ref={four} />
             </div>
         </div>
       </div>)
